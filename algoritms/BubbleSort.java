bfc2f64c55f1b6725a0ecb1e5d17557c0206bbe5
laura.kaliyeva <laura.kaliyeva@samgau.com>
2019-07-22 15:46:02 +0600

    sorting algorithms problem

---
+++ algoritms/BubbleSort.java
@@ -0,0 +1,17 @@
+package algoritms;
+
+public class BubbleSort {
+
+    public static void bubbleSort(int[] arr) {
+        for (int i = arr.length - 1; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                int a = arr[j];
+                int b = arr[j+1];
+                if (a > b) {
+                    arr[j] = b;
+                    arr[j+1] = a;
+                }
+            }
+        }
+    }
+}
