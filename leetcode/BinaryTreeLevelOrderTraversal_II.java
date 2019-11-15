e2e51e823a64ae09430ade6c2b9f311261bc16b5
Laura K <laura.kaliyeva@nu.edu.kz>
2019-11-15 12:28:17 +0600

    leetcode: trees

---
+++ leetcode/BinaryTreeLevelOrderTraversal_II.java
@@ -0,0 +1,38 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreeLevelOrderTraversal_II {
+
+    /*
+    * Given a binary tree, return the bottom-up level order traversal of its nodes' values.
+    *
+    * Example           Input: 3
+    *                         / \
+    *                        9  20
+    *                          /  \
+    *                         15   7
+    *                   Outout: [
+    *                            [15,7],
+    *                            [9,20],
+    *                            [3]
+    *                           ]
+    */
+
+    public static List<List<Integer>> levelOrderBottom(TreeNode root) {
+        return fillList(root, new ArrayList<>(), 0);
+    }
+
+    private static List<List<Integer>> fillList(TreeNode node, List<List<Integer>> lists, int idx) {
+        if (node != null) {
+            while (idx > lists.size() - 1) {
+                lists.add(0, new ArrayList<>());
+            }
+            lists.get(lists.size() - 1 - idx).add(node.val);
+            fillList(node.left, lists, idx + 1);
+            fillList(node.right, lists, idx + 1);
+        }
+        return lists;
+    }
+}
