7e1c5fe58173447be88937dc286de8589d782f55
Laura K <laura.kaliyeva@nu.edu.kz>
2019-11-26 12:34:15 +0600

    leetcode: trees

---
+++ leetcode/ConvertBSTToGreaterTree.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+public class ConvertBSTToGreaterTree {
+
+    /*
+    * Given a Binary Search Tree, convert it to a Greater Tree such that
+    * every key of the original BST is changed to the original key plus
+    * sum of all keys greater than the original key in BST.
+    *
+    * Example           Input: 5            Output: 18
+    *                         / \                  /  \
+    *                        2   13               20   13
+    */
+
+    private int sum;
+
+    public TreeNode convertBST(TreeNode root) {
+        if (root == null) return null;
+        convertBST(root.right);
+        sum += root.val;
+        root.val += sum - root.val;
+        convertBST(root.left);
+        return root;
+    }
+}
