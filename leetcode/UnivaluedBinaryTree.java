b7e6da144d13cd0c8e8d802cc0128f8821a02b6e
Laura K <laura.kaliyeva@nu.edu.kz>
2019-12-04 15:27:18 +0600

    leetcode: trees

---
+++ leetcode/UnivaluedBinaryTree.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+/*
+ * A binary tree is univalued if every node in the tree has the same value.
+ * Return true if and only if the given tree is univalued.
+ *
+ * Example          Input: [1,1,1,1,1,null,1]
+ *                  Output: true
+ *
+ *                  Input: [2,2,2,5,2]
+ *                  Output: false
+*/
+
+public class UnivaluedBinaryTree {
+
+    public boolean isUnivalTree(TreeNode root) {
+        if (root == null) return true;
+        if (root.left != null && root.left.val != root.val) {
+            return false;
+        }
+        if (root.right != null && root.right.val != root.val) {
+            return false;
+        }
+        return isUnivalTree(root.left) && isUnivalTree(root.right);
+    }
+}
