551fbf12ed7e5624ee07ca514f8f54749c5b3d51
Laura K <laura.kaliyeva@nu.edu.kz>
2019-12-05 17:13:26 +0600

    leetcode: maths

---
+++ leetcode/ExcelSheetColumnTitle.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/*
+ * Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+ *
+ * Example          Input: 1            Output: "A"
+ *                  Input: 2            Output: "B"
+ *                  Input: 28           Output: "AB"
+ *                  Input: 701          Output: "ZY"
+*/
+
+public class ExcelSheetColumnTitle {
+
+    public String convertToTitle(int n) {
+        return n == 0 ? "" : convertToTitle((n - 1) / 26) + (char) ('A' + (n - 1) % 26);
+    }
+}
