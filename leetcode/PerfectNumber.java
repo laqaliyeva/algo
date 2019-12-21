5e80ae50fc77fbcdd038ef0ffb01fc331e902538
Laura K <laura.kaliyeva@nu.edu.kz>
2019-12-21 13:44:58 +0600

    maths

--- leetcode/PerfectNumber.java
+++ leetcode/PerfectNumber.java
@@ -1,5 +1,14 @@
 package leetcode;
 
+/*
+ * We define the Perfect Number is a positive integer that is equal to
+ * the sum of all its positive divisors except itself. Now, given an integer n,
+ * write a function that returns true when it is a perfect number and false when it is not.
+ *
+ * Example          Input: 28
+ *                  Output: true (1 + 2 + 4 + 7 + 14)
+*/
+
 public class PerfectNumber {
 
     public boolean checkPerfectNumber(int num) {
