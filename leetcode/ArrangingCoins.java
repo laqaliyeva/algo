bc3209c0b7cfb18e9cba0e2ce2fbd92bcc480a4e
Laura K <laura.kaliyeva@nu.edu.kz>
2019-12-21 13:49:15 +0600

    maths

--- leetcode/ArrangingCoins.java
+++ leetcode/ArrangingCoins.java
@@ -1,5 +1,19 @@
 package leetcode;
 
+/*
+ * You have a total of n coins that you want to form in a staircase shape,
+ * where every k-th row must have exactly k coins. Given n, find the total number
+ * of full staircase rows that can be formed.
+ *
+ * Example          Input: 8
+ *                  Output: 3
+ *
+ * Explanation: 1st row *
+ *              2nd row **
+ *              3rd row ***
+ *              4th row **  - incomplete
+*/
+
 public class ArrangingCoins {
 
     public int arrangeCoins(int n) {
