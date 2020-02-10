99b094a59280bb2d110b1ba20b1855ca10edc3f3
Laura K <laura.kaliyeva@nu.edu.kz>
2020-02-10 12:09:41 +0600

    hackerrank: strings

---
+++ hackerank/RepeatedString.java
@@ -0,0 +1,28 @@
+package hackerank;
+
+/*
+ * https://www.hackerrank.com/challenges/repeated-string
+*/
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class RepeatedString {
+
+    static long repeatedString(String s, long n) {
+        if (!s.contains("a")) return 0;
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < s.length(); i ++) {
+            if (s.charAt(i) == 'a') {
+                set.add(i);
+            }
+        }
+        long result = n / s.length() * set.size();
+        for (int i = 0; i < n % s.length(); i ++) {
+            if (set.contains(i)) {
+                result++;
+            }
+        }
+        return result;
+    }
+}
