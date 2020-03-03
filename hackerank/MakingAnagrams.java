6d8857f3da8c2bfe7101e9238bdaa73097362ebd
Laura K <laura.kaliyeva@nu.edu.kz>
2020-03-03 14:35:04 +0600

    hackerrank: strings

---
+++ hackerank/MakingAnagrams.java
@@ -0,0 +1,25 @@
+package hackerank;
+
+/*
+ * https://www.hackerrank.com/challenges/ctci-making-anagrams
+*/
+
+public class MakingAnagrams {
+
+    static int makeAnagram(String a, String b) {
+        int[] alphabet = new int[26];
+        for (char c : a.toCharArray()) {
+            alphabet[c - 'a']++;
+        }
+        for (char c : b.toCharArray()) {
+            alphabet[c - 'a']--;
+        }
+        int ans = 0;
+        for (int i : alphabet) {
+            if (i != 0) {
+                ans += Math.abs(i);
+            }
+        }
+        return ans;
+    }
+}
