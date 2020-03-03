0468cc3da3680eb142d5b1f864034c6197e9163c
Laura K <laura.kaliyeva@nu.edu.kz>
2020-03-03 14:05:48 +0600

    hackerrank: strings

---
+++ hackerank/AlternatingCharacters.java
@@ -0,0 +1,18 @@
+package hackerank;
+
+/*
+ * https://www.hackerrank.com/challenges/alternating-characters
+*/
+
+public class AlternatingCharacters {
+
+    static int alternatingCharacters(String s) {
+        int ans = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
