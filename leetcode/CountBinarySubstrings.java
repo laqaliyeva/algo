e8f4aa0514feff1d79dc1f5904e32c86f0cfeff7
Laura K <laura.kaliyeva@nu.edu.kz>
2020-03-05 15:18:10 +0600

    leetcode: strings

--- leetcode/CountBinarySubstrings.java
+++ leetcode/CountBinarySubstrings.java
@@ -1,9 +1,12 @@
 package leetcode;
 
 /*
- * Give a string s, count the number of non-empty (contiguous) substrings that have the same number
+ * Given a string s, count the number of non-empty (contiguous) substrings that have the same number
  * of 0's and 1's, and all the 0's and all the 1's in these substrings are grouped consecutively.
  * Substrings that occur multiple times are counted the number of times they occur.
+ *
+ * Example:         Input: "00110011"
+ *                  Output: 6 (01, 0011, 10, 1100, 01, 0011)
  */
 
 import java.util.ArrayList;
