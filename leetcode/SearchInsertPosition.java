66bddbbed99993891ba6ed810e5cc3117b1ce5b9
Laura K <laura.kaliyeva@nu.edu.kz>
2020-06-11 10:23:05 +0600

    leetcode: refactor SearchInsertPosition to O(log n) solution

--- leetcode/SearchInsertPosition.java
+++ leetcode/SearchInsertPosition.java
@@ -1,16 +1,31 @@
 package leetcode;
 
+/*
+ * Given a sorted array and a target value, return the index if the target is found.
+ * If not, return the index where it would be if it were inserted in order.
+ * You may assume no duplicates in the array.
+ *
+ * Example          Input: [1,3,5,6], 5
+ *                  Output: 2
+ *
+ *                  Input: [1,3,5,6], 2
+ *                  Output: 1
+*/
+
 public class SearchInsertPosition {
 
     public static int searchInsertPosition(int[] nums, int target) {
-        int index = 0;
-        while (index < nums.length && index >= 0) {
-            if (target <= nums[index]) {
-                return index;
+        int start = 0, end = nums.length - 1;
+        int mid = start + (end - start) / 2;
+        while (start <= end) {
+            if (nums[mid] == target) return mid;
+            if (nums[mid] < target) {
+                start = mid + 1;
             } else {
-                index++;
+                end = mid - 1;
             }
+            mid = start + (end - start) / 2;
         }
-        return index;
+        return mid;
     }
 }
