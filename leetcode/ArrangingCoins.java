6d06e7dacee53c492dc5699244343a0b00d41be9
Laura K <laura.kaliyeva@nu.edu.kz>
2020-07-02 12:33:55 +0600

    leetcode: maths - mistake in ArrangingCoins

--- leetcode/ArrangingCoins.java
+++ leetcode/ArrangingCoins.java
@@ -17,12 +17,11 @@ package leetcode;
 public class ArrangingCoins {
 
     public int arrangeCoins(int n) {
-        if (n == 0 || n == 1) return n;
         int i = 0;
-        while (n > 0) {
+        while (n > i) {
             i++;
             n -= i;
         }
-        return --i;
+        return i;
     }
 }
